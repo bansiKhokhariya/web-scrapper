511c025cf096cba1254d35e628e6aa9c8496af6c
bansi-hkapps <bansikhokhariya59@gmail.com>
2024-09-04 11:14:14 +0530

    567

--- src/Config/cronJobs.js
+++ src/Config/cronJobs.js
@@ -3,10 +3,8 @@ const { finnScrapeData , testCronAPI } = require('../Controllers/FinnDataControl
 const { olxScrapeData } = require('../Controllers/OlxController');
 const { updateMissingPhoneNumbers } = require('../Controllers/fetchNumberController'); // Adjust the path as needed
 
-
-
 // Schedule the updateMissingPhoneNumbers to run every minute
-cron.schedule('52 10 * * *', async () => {
+cron.schedule('30 11 * * *', async () => {
     console.log('Running test cron every minute');
     await testCronAPI();
 });
